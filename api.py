b41c24935a1ce0b0c3d6b16a8905eea18fbff98b
huaishu <1727987834@qq.com>
2026-07-06 14:54:26 +0800

    add id field

--- api.py
+++ api.py
@@ -2,12 +2,19 @@ import json
 from pathlib import Path
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi import HTTPException
 
 app = FastAPI()
 
 DATA_FILE = Path("papers.json")
 
 class PaperCreate(BaseModel):
+    title: str
+    authors: str
+    year: int
+    keywords: list[str]
+
+class PaperUpdate(BaseModel):
     title: str
     authors: str
     year: int
@@ -55,6 +62,7 @@ def create_paper(paper:PaperCreate):
     papers = load_papers()
 
     new_paper={
+        "id": get_next_id(papers),
         "title": paper.title,
         "authors": paper.authors,
         "year": paper.year,
@@ -64,3 +72,53 @@ def create_paper(paper:PaperCreate):
     papers.append(new_paper)
     save_papers(papers)
     return {"message": "paper created", "paper": new_paper}
+
+def get_next_id(papers):
+    if not papers:
+        return 1
+    max_id = max(paper.get("id",0) for paper in papers)
+    return max_id + 1
+
+@app.get("/papers/{paper_id}")
+def get_paper(paper_id: int):
+    papers = load_papers()
+
+    for paper in papers:
+        if paper['id'] == paper_id:
+            return paper
+
+    raise HTTPException(status_code=404, detail="Paper not found")
+
+@app.put("papers/{paper_id}")
+def update_paper(paper_id: int, paper: PaperUpdate):
+    papers = load_papers()
+
+    for index, old_paper in enumerate(papers):
+        if old_paper["id"] == paper_id:
+            updated_paper = {
+                "id": paper_id,
+                "title": paper.title,
+                "authors": paper.authors,
+                "year": paper.year,
+                "keywords": paper.keywords,
+            }
+
+            papers[index] = updated_paper
+            save_papers(papers)
+
+            return {"message": "paper updated","paper": updated_paper}
+
+    raise HTTPException(status_code=404,detail="paper not found")
+
+@app.detele("/papers/{paper_id}")
+def delete_paper(paper_id: int):
+    papers = load_papers()
+
+    for index, paper in enumerate(papers):
+        if paper["id"] == paper_id:
+            deleted_paper = papers.pop(index)
+            save_papers(papers)
+
+            return {"message": "paper deteled", "paper": deleted_paper}
+
+    raise HTTPException(status_code=404,detail="Paper not found")
